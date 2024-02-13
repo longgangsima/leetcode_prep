52acbe8dd119e54c915c1ec8a321ea9609d5a035
longgangsima <longgangsima@gmail.com>
2024-02-13 09:48:30 -0500

    Search in Rotated Sorted Array

---
+++ 33.search-in-rotated-sorted-array.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=33 lang=javascript
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  // 1. 二分查找
+  // 2. 二分查找的时候，判断哪一边是有序的
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    //  左边有序
+    if (nums[left] <= nums[mid]) {
+      if (target >= nums[left] && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    // 右边有序
+    else {
+      if (target > nums[mid] && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+  return -1;
+};
+// @lc code=end
