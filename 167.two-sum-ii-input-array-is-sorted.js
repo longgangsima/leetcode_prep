3e7c40cf936a6b56b78f892ea2deae381e9b0633
longgangsima <longgangsima@gmail.com>
2024-02-20 23:45:52 -0500

    two-sum II, use two points

---
+++ 167.two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=167 lang=javascript
+ *
+ * [167] Two Sum II - Input Array Is Sorted
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  // use two points to solve this problem
+  let left = 0;
+  let right = numbers.length - 1;
+  while (left < right) {
+    if (numbers[left] + numbers[right] === target) {
+      return [left + 1, right + 1];
+    } else if (numbers[left] + numbers[right] < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return [-1, -1];
+};
+// @lc code=end
